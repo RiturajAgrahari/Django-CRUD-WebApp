38865f23ed82f86045679890f07adcf8151d46bf
RiturajAgrahari <agr.rituraj@gmail.com>
2024-01-10 21:41:14 +0530

    creating login/logout

--- MyFirstWebApp/urls.py
+++ MyFirstWebApp/urls.py
@@ -4,6 +4,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login_user, name='Login'),
     path('logout/', views.logout_user, name='Logout'),
 ]
