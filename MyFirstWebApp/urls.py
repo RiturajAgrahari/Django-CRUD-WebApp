7b04c965bf55934e32e9b76bf6de57d5775b4b65
RiturajAgrahari <agr.rituraj@gmail.com>
2024-01-11 22:33:42 +0530

    project completed

--- MyFirstWebApp/urls.py
+++ MyFirstWebApp/urls.py
@@ -5,4 +5,9 @@ from django.urls import include, path
 urlpatterns = [
     path('', views.home, name='home'),
     path('logout/', views.logout_user, name='Logout'),
+    path('register/', views.register_user, name='Register'),
+    path('record/<int:pk>', views.customer_record, name='record'),
+    path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
+    path('add_record/', views.add_record, name='add_record'),
+    path('update_record/<int:pk>', views.update_record, name='update_record'),
 ]
